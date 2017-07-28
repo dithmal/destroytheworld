6b4eb9fc7882f0513416b0f012b3b6a9f2163ab1
Nadila Dithmal <NadilaDithmal@nadilas-macbook-pro.local>
2017-07-28 08:01:58 +0530

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Nadila Dithmal on 7/28/17.
+//  Copyright © 2017 Nadila Dithmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
